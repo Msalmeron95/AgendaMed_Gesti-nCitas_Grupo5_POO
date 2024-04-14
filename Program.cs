756e298afaee1d0c0d14d80a9ceb66b10bed34bf
Msalmeron95 <140337045+Msalmeron95@users.noreply.github.com>
2024-04-13 21:58:09 -0400

    Adding Main structure

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyNamespace
+{
+    public class MyClass
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
